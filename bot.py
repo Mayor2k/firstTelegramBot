d36b45219eda76e067bd79e2c9d9392eb6eadf57
Mayor2k <Maxim27r@outlook.com>
2019-09-16 23:13:10 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,24 @@
+import telebot
+from telebot import apihelper
+
+
+apihelper.proxy = {'http':'http://195.201.137.246:1080'}
+
+bot = telebot.TeleBot('674637341:AAGg5XJ5LMwwLL0ft2vnRdwX4ToinWxW-6Q')
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard1.row('Привет', 'Пока')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+        bot.send_photo(message.chat.id, "https://images.unsplash.com/photo-1481819613568-3701cbc70156?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80")
+
+
+bot.polling()
