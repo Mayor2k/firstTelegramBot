ed56138002947b0a73c0ecde46902ff260cd80ff
Mayor2k <Maxim27r@outlook.com>
2019-09-22 23:11:18 +0300

    Add a proxy

--- main.py
+++ main.py
@@ -1,31 +1,66 @@
 import telebot
-<<<<<<< HEAD
+import time_helper
+import sqlite3
+
 #more socks proxy:
-=======
 #more proxy:
->>>>>>> a8a371582159375a3c51fcc4c152a3cbf205ff02
 #https':'socks5://login:pass@orbtl.s5.opennetwork.cc:port
 #127.0.0.1:9150
 
 telebot.apihelper.proxy ={'https':'socks5://14611055481:U777Vluhz8@orbtl.s5.opennetwork.cc:999'}
 
 bot = telebot.TeleBot('674637341:AAGg5XJ5LMwwLL0ft2vnRdwX4ToinWxW-6Q')
-keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('Привет', 'Пока')
+user_id = bot.get_me().id
+
+keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard.row('1', '2','3')
+
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+    delivery_days_list = time_helper.delivery_days_list(time_helper.get_current_date()[0],
+                                                        time_helper.get_current_date()[1],
+                                                        time_helper.get_current_date()[2])
+    conn = sqlite3.connect(r"E:\Vault\Projects\telegramBot\data.sqlite")
+    cursor = conn.cursor()
+
+    for x in range(5):
+        if call.data == str(x)+'day':
+            date = delivery_days_list[x][0]+"."+delivery_days_list[x][2]+"."+delivery_days_list[x][3]
+            try:
+                cursor.execute("INSERT INTO delivery (id,delivery_method, delivery_date) VALUES (:id,'delivery', :date)", {"date":date,"id":user_id})
+            except sqlite3.IntegrityError:
+                cursor.execute("UPDATE delivery SET delivery_date=:date  WHERE id=:id", {"date":date,"id":user_id})
+            finally:
+                conn.commit()
+                conn.close()
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+    bot.send_message(message.chat.id, 'Welcome to the our telegram bot. Here you can track your order. What do you what to do?\n'
+                                      '1 - Choose date and place for your order\n'
+                                      '2 - Pickup the order\n'
+                                      '3 - Track the order\n', reply_markup=keyboard)
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    if message.text.lower() == '1':
+        #0-day,1-weekday,2-month,3-year
+        delivery_days_list = time_helper.delivery_days_list(time_helper.get_current_date()[0],
+                                                            time_helper.get_current_date()[1],
+                                                            time_helper.get_current_date()[2])
+        keyboard1 = telebot.types.InlineKeyboardMarkup()
+        for x in range(5):
+            button = telebot.types.InlineKeyboardButton(
+                text=delivery_days_list[x][0]+"th "+delivery_days_list[x][2]+" - "+delivery_days_list[x][1],
+                callback_data=str(x)+"day")
+            keyboard1.add(button)
+
+        bot.send_message(message.chat.id, 'Choose day, when you can get the order(except days-off)',reply_markup=keyboard1)
+
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'Прощай, создатель')
-        bot.send_photo(message.chat.id, "https://images.unsplash.com/photo-1481819613568-3701cbc70156?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80")
     else:
-        bot.send_message(message.chat.id, "Извините, я вас не понял")
+        bot.send_message(message.chat.id, "Sorry, I don't understand you", reply_markup=keyboard)
 
 
-bot.polling()
+bot.polling(none_stop=True)
---
+++ time_helper.py
@@ -0,0 +1,68 @@
+import time
+import calendar
+
+
+def _fix_date(day, month, year):
+    return_array = [day, month, year]
+    return_array[1] += 1
+    if return_array[1] > 12:
+        return_array[1] = 1
+        return_array[2] += 1
+    return_array[0] = return_array[0] + 1 - calendar.monthrange(return_array[2], return_array[1])[1]
+    if return_array[0] == 0:
+        return_array[0] = 1
+    return return_array
+
+
+def _format_months_and_weekdays(weekday, month):
+    weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat","Sun"]
+    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    return weekdays[weekday], months[month-1]
+
+
+def delivery_days_list(day, month, year):
+    global weekday1
+    return_array = [[0] * 4] * 5
+
+    for i in range(5):
+        day1 = day + 7 if i == 0 else return_array[i - 1][0] + 1
+        month1 = month if i == 0 else return_array[i - 1][2]
+        year1 = year if i == 0 else return_array[i - 1][3]
+        try:
+            weekday1 = calendar.weekday(year1, month1, day1)
+        except ValueError:
+            day1, month1, year1 = _fix_date(day1, month1, year1)[0], _fix_date(day1, month1, year1)[1], \
+                                  _fix_date(day1, month1, year1)[2]
+            weekday1 = calendar.weekday(year1, month1, day1)
+        finally:
+            if weekday1 == 5:
+                if day1 > calendar.monthrange(2019, 5)[1]:
+                    day1, month1, year1 = _fix_date(day1 + 2, month1, year1)[0], \
+                                          _fix_date(day1 + 2, month1, year1)[1], \
+                                          _fix_date(day1 + 2, month1, year1)[2]
+                else:
+                    day1 += 2
+                weekday1 = 0
+            elif weekday1 == 6:
+                if day1 > calendar.monthrange(2019, 5)[1]:
+                    day1, month1, year1 = _fix_date(day1 + 1, month1, year1)[0], \
+                                          _fix_date(day1 + 1, month1, year1)[1], \
+                                          _fix_date(day1 + 1, month1, year1)[2]
+                else:
+                    day1 += 1
+                weekday1 = 0
+
+        return_array[i] = [day1, weekday1, month1, year1]
+
+    for x in range(5):
+        weekday = _format_months_and_weekdays(return_array[x][1],return_array[x][2])[0]
+        month = _format_months_and_weekdays(return_array[x][1],return_array[x][2])[1]
+        return_array[x] = [str(return_array[x][0]), weekday,month, str(return_array[x][3])]
+    return return_array
+
+
+def get_current_date():
+    date = time.strftime('%d,' + '%m,' + '%Y', time.gmtime())
+    date = date.split(",")
+    date = [int(date[x]) for x in range(3)]
+    return date
